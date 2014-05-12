17e6aabd0ab31b81dbdf7f57ce31b4909f5bca41
Bogdan <bogdan@MacBook-Air-Bogdan.local>
2014-05-12 16:37:27 +0400

    Initial Commit

---
+++ Hypermarka/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hypermarka
+//
+//  Created by Bogdan on 12.05.14.
+//  Copyright (c) 2014 mifors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
